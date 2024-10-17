3b965a8dc4154e92bbe73b6ceec904559e920cce
brunadelrio <brunaadelrio@gmail.com>
2024-10-17 16:37:39 -0300

    feat: rota post cadastro de pessoa

--- index.js
+++ index.js
@@ -13,7 +13,36 @@ app.use(
 
 app.use(express.json())
 
-// rota/endpoint inicial
+// rotas da API
+app.post('/person', async (req, res) => {
+    // req.body
+
+    // {name: "Bruna", salary: 5000, approved: false}
+    const {name, salary, approved} = req.body
+
+    if (!name) {
+        res.status(422).json({error: "O nome é obrigatório"})
+    }
+    if (!salary) {
+        res.status(422).json({error: "O salário é obrigatório"})
+    }
+
+    const person = {
+        name,
+        salary,
+        approved
+    }
+
+    // criando dados
+    try {
+        await Person.create(person)
+        res.status(201).json({message: "Pessoa inserida no sistema com sucesso!"})
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
+})
+
+// rota inicial / endpoint
 app.get('/', (req, res) => {
     res.json({ message: "Oi Express!" })
 })
